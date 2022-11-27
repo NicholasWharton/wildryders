c39a568f0a05e4a852e8fbd3663b54aa2e282846
Nicholas Wharton <nickggrae@gmail.com>
2022-11-27 13:52:03 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_MNzd4CEvv',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '22ja7n5jqonggvnpn2g63b3i5s',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             'us-east-2'          // e.g. us-east-2
+        userPoolId:         'us-east-1_TN5FIOTRK',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '6mgnrk5567ejpsqkage4g4trgp',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          'https://70v8qk0ml1.execute-api.us-east-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
