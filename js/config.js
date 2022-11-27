b283dd8baae141660e00e6d03a8d89cd233db9f5
Nicholas Wharton <nickggrae@gmail.com>
2022-11-27 14:25:05 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://70v8qk0ml1.execute-api.us-east-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://e0z9nzcaqj.execute-api.us-east-1.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
